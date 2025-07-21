ba186b585ac31dd5517b29e95154789a98525907
Matee Saafi <matiullahsafibbc@gmail.com>
2025-07-21 12:01:18 +0430

    add posts

---
+++ src/collections/Posts.ts
@@ -0,0 +1,68 @@
+import { slugField } from '@/fields/slug'
+import type { CollectionConfig } from 'payload'
+
+export const Posts: CollectionConfig = {
+  slug: 'posts',
+  admin: {
+    useAsTitle: 'title',
+    defaultColumns: ['title', 'slug', 'updatedAt'],
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'image',
+      type: 'upload',
+      relationTo: 'media',
+      admin: {
+        description: 'Upload an image for the post',
+      },
+    },
+    {
+      name: 'content',
+      type: 'textarea',
+      required: true,
+    },
+    {
+      name: 'author',
+      type: 'relationship',
+      admin: {
+        position: 'sidebar',
+      },
+      relationTo: 'users',
+    },
+    {
+      name: 'publishedAt',
+      type: 'date',
+      admin: {
+        date: {
+          pickerAppearance: 'dayAndTime',
+        },
+        position: 'sidebar',
+      },
+      hooks: {
+        beforeChange: [
+          ({ siblingData, value }) => {
+            if (siblingData._status === 'published' && !value) {
+              return new Date()
+            }
+            return value
+          },
+        ],
+      },
+    },
+    ...slugField(),
+  ],
+  versions: {
+    drafts: {
+      autosave: {
+        interval: 5000,
+      },
+      schedulePublish: true,
+    },
+    maxPerDoc: 50,
+  },
+}
